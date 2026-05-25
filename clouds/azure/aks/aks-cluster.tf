e82ec67826d10ea29c0b4099a185e5c464b99bba
jcloudcodes <jcloudcodes@gmail.com>
2026-05-25 18:33:25 -0500

    fix(azure-aks): format terraform files for github actions

--- clouds/azure/aks/aks-cluster.tf
+++ clouds/azure/aks/aks-cluster.tf
@@ -1,23 +1,23 @@
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = local.aks_cluster_name
-  location            = var.azure_location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = local.dns_prefix
-  kubernetes_version  = var.cluster_version
-  sku_tier            = var.sku_tier
+  name                    = local.aks_cluster_name
+  location                = var.azure_location
+  resource_group_name     = var.resource_group_name
+  dns_prefix              = local.dns_prefix
+  kubernetes_version      = var.cluster_version
+  sku_tier                = var.sku_tier
   private_cluster_enabled = var.private_cluster_enabled
   private_dns_zone_id     = var.private_cluster_enabled ? var.private_dns_zone_id : null
 
   default_node_pool {
     temporary_name_for_rotation = var.node_pool_temporary_name_for_rotation
-    name                 = var.node_pool_name
-    vm_size              = var.node_pool_vm_size
-    vnet_subnet_id       = var.vnet_subnet_id
-    type                 = var.node_pool_type
-    os_disk_size_gb      = var.node_pool_os_disk_size_gb
-    os_disk_type         = var.node_pool_os_disk_type
-    max_pods             = var.node_pool_max_pods
-    zones                = var.node_pool_zones
+    name                        = var.node_pool_name
+    vm_size                     = var.node_pool_vm_size
+    vnet_subnet_id              = var.vnet_subnet_id
+    type                        = var.node_pool_type
+    os_disk_size_gb             = var.node_pool_os_disk_size_gb
+    os_disk_type                = var.node_pool_os_disk_type
+    max_pods                    = var.node_pool_max_pods
+    zones                       = var.node_pool_zones
     auto_scaling_enabled = var.node_pool_enable_auto_scaling
     node_count           = var.node_pool_enable_auto_scaling ? null : var.node_pool_desired_size
     min_count            = var.node_pool_enable_auto_scaling ? var.node_pool_min_size : null
--- clouds/azure/aks/aks.auto.tfvars
+++ clouds/azure/aks/aks.auto.tfvars
@@ -14,17 +14,17 @@ vnet_subnet_id = "/subscriptions/b4839350-90c8-44f1-a077-af30199bbd11/resourceGr
 service_cidr   = "172.20.0.0/16"
 dns_service_ip = "172.20.0.10"
 
-node_pool_name                = "system"
+node_pool_name                         = "system"
 node_pool_temporary_name_for_rotation = "systemtmp"
-node_pool_vm_size             = "Standard_DC2as_v5"
-node_pool_os_disk_size_gb     = 128
-node_pool_os_disk_type        = "Managed"
-node_pool_enable_auto_scaling = true
-node_pool_desired_size        = 2
-node_pool_min_size            = 2
-node_pool_max_size            = 4
-node_pool_max_pods            = 30
-node_pool_zones               = ["1", "3"]
+node_pool_vm_size                      = "Standard_DC2as_v5"
+node_pool_os_disk_size_gb              = 128
+node_pool_os_disk_type                 = "Managed"
+node_pool_enable_auto_scaling          = true
+node_pool_desired_size                 = 2
+node_pool_min_size                     = 2
+node_pool_max_size                     = 4
+node_pool_max_pods                     = 30
+node_pool_zones                        = ["1", "3"]
 
 network_plugin    = "azure"
 network_policy    = "azure"
