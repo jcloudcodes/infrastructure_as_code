1daa7078931d6763541940e6f1197e1fedfc316b
Eghosa Agunu <jcloudcodes@gmail.com>
2026-09-12 11:59:45 -0500

    Remove obsolete VNet variables

--- clouds/azure/aks/aks-variables.tf
+++ clouds/azure/aks/aks-variables.tf
@@ -34,16 +34,6 @@ variable "dns_prefix" {
   default     = null
 }
 
-variable "vnet_id" {
-  description = "Resource ID of the VNet used by AKS. The AKS managed identity receives Network Contributor on this scope."
-  type        = string
-}
-
-variable "vnet_subnet_id" {
-  description = "Subnet ID where the AKS node pool will run"
-  type        = string
-}
-
 variable "service_cidr" {
   description = "CIDR block for Kubernetes services"
   type        = string
